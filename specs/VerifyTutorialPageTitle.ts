b55a83bffb89e261d107819858e9aef5f179028c
skamblem@gmail.com <skamblem@gmail.com>
2018-09-11 12:02:58 +0530

    Minor Change

--- specs/VerifyTutorialPageTitle.ts
+++ specs/VerifyTutorialPageTitle.ts
@@ -5,14 +5,12 @@ describe('Tutorial Page', function() {
     //Globally
     var basePage =new BasePage();
     var tutorialPage=new TutorialPage();
-
-    
     
 	it('Title verifications of Tutorial Page', function() {
-    basePage.NavigateToTutorialPage();
-      tutorialPage.VerifyTutoraialPageTitle().then(function (title) {
-      console.log("The title of Tutorial Page is : "+title);
-      expect(title).toBe("Protractor - end-to-end testing for AngularJS");
+       // basePage.NavigateToTutorialPage();
+        tutorialPage.VerifyTutoraialPageTitle().then(function (title) {
+        console.log("The title of Tutorial Page is : "+title);
+        expect(title).toBe("Protractor - end-to-end testing for AngularJS");
       });
 	});
 
